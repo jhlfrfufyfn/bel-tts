dbc17f7d64e5b3ac804ef42d7184663ffbd82551
jhlfrfufyfn <jhlfrfufyfn@gmail.com>
2022-04-22 03:34:00 +0300

    feat: my training code and configs added

--- TTS/tts/utils/text/cleaners.py
+++ TTS/tts/utils/text/cleaners.py
@@ -53,6 +53,9 @@ def replace_symbols(text, lang="en"):
         text = text.replace("&", " et ")
     elif lang == "pt":
         text = text.replace("&", " e ")
+    elif lang == "be":
+        text = text.replace('i', 'і');
+        text = text.replace("’", "'");
     return text
 
 
@@ -143,3 +146,10 @@ def multilingual_cleaners(text):
     text = remove_aux_symbols(text)
     text = collapse_whitespace(text)
     return text
+
+def belarusian_cleaners(text):
+    """Pipeline for Belarusian text"""
+    text = lowercase(text)
+    text = replace_symbols(text, lang="be")
+    text = remove_aux_symbols(text)
+    text = collapse_whitespace(text)
