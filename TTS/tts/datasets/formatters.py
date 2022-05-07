def06c5c907cc8ac628550ad37cd98482380ff20
jhlfrfufyfn <jhlfrfufyfn@gmail.com>
2022-05-08 01:23:05 +0300

    chore: updated batch sizes

--- TTS/tts/datasets/formatters.py
+++ TTS/tts/datasets/formatters.py
@@ -493,3 +493,15 @@ def kokoro(root_path, meta_file, **kwargs):  # pylint: disable=unused-argument
             text = cols[2].replace(" ", "")
             items.append({"text": text, "audio_file": wav_file, "speaker_name": speaker_name})
     return items
+
+def commonvoice_be(root_path, meta_file, **kwargs):  # pylint: disable=unused-argument
+    txt_file = os.path.join(root_path, meta_file)
+    items = []
+    speaker_name = "maledataset1"
+    with open(txt_file, "r", encoding="utf-8") as ttf:
+        for line in ttf:
+            cols = line.split("|")
+            wav_file = os.path.join(root_path, "wavs", cols[0])
+            text = cols[1]
+            items.append({"text": text, "audio_file": wav_file, "speaker_name":speaker_name})
+    return items
